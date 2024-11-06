54a161088dbf020ea3b5ed7e2fa700f81b989dd4
Raphael Katayama <rapha.katayama@gmail.com>
2024-11-06 00:53:11 -0300

    Lista de Compras

---
+++ Beecrowd/lista_de_compras.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_ITEMS 1000
+#define MAX_LENGTH 21
+
+int compare(const void *a, const void *b) {
+    return strcmp(*(const char **)a, *(const char **)b);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    getchar(); 
+
+    for (int i = 0; i < n; i++) {
+        char line[20000];
+        fgets(line, sizeof(line), stdin);
+
+        char *items[MAX_ITEMS];
+        int item_count = 0;
+
+        char *token = strtok(line, " \n");
+        while (token != NULL) {
+            items[item_count++] = token;
+            token = strtok(NULL, " \n");
+        }
+
+        qsort(items, item_count, sizeof(char *), compare);
+
+        printf("%s", items[0]);
+        for (int j = 1; j < item_count; j++) {
+            if (strcmp(items[j], items[j - 1]) != 0) {
+                printf(" %s", items[j]);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
