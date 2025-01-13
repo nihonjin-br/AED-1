f9dfd3f0ba2b63137f179f082ac9f52f5e6ab22b
Raphael Katayama <rapha.katayama@gmail.com>
2025-01-13 14:04:38 -0300

    Beecrowd | 1241 - Encaixa ou Não II

---
+++ Beecrowd/encaixa_II.c
@@ -0,0 +1,24 @@
+// Beecrowd | 1241 - Encaixa ou Não II //
+
+#include <string.h>
+#include <stdio.h>
+
+int main(){
+    int N;
+    char A[1001], B[1001];
+
+    scanf("%d\n", &N);
+
+    for(int i = 0; i < N; ++i){
+        scanf("%s %s\n", &A, &B);
+
+        if(strlen(B) > strlen(A))   printf("nao encaixa\n");
+        else{
+            strcpy(A, A + (strlen(A) - strlen(B)));
+            if(!strcmp(A, B))       printf("encaixa\n");
+            else                    printf("nao encaixa\n");
+        }
+    }
+
+    return 0;
+}
