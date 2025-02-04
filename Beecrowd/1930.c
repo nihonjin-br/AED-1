0d3d780394274cb3742afc11f879950d0e88e069
Raphael Katayama <rapha.katayama@gmail.com>
2025-02-04 17:09:11 -0300

    Beecrowd | 1930 - Tomadas

---
+++ Beecrowd/1930.c
@@ -0,0 +1,13 @@
+// Beecrowd | 1930 - Tomadas //
+
+#include <stdio.h>
+ 
+int main() { 
+    int T1, T2, T3, T4, totalTomadas;
+
+    scanf("%d %d %d %d", &T1, &T2, &T3, &T4);
+    totalTomadas = (T1 + T2 + T3 + T4) - 3;
+    printf("%d\n", totalTomadas);
+
+    return 0;
+}
